01098cb2382c9e246c622e845bc63b2dd05005f8
ian <highmeme10@gmail.com>
2021-11-19 09:24:23 +0300

    DB Seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,6 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Duka.Businesses.Business
+alias Duka.Businesses.Products
+
