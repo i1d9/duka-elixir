f2d395346b395350dffa677e870d7d340658bd3f
ian <highmeme10@gmail.com>
2021-11-24 21:47:48 +0300

    Subdomain Functional Plug

--- lib/duka_web/controllers/subdomain/product_controller.ex
+++ lib/duka_web/controllers/subdomain/product_controller.ex
@@ -2,19 +2,23 @@ defmodule DukaWeb.Subdomain.ProductController do
   use DukaWeb, :controller
 
   alias Duka.Businesses
+  plug :get_valid_business when action in [:index, :show]
 
-  def index(conn, _params) do
-
-
-    if Businesses.get_business_by_name(conn.private[:subdomain]) do
-      text(conn, "Subdomain home page for #{conn.private[:subdomain]}")
+  defp get_valid_business(conn, _opts) do
+    if business = Businesses.get_business_by_name(conn.private[:subdomain]) do
+      conn
+      |> assign(:business, business)
     else
         redirect(conn, external: Routes.duka_url(conn, :index))
     end
+  end
+
+  def index(conn, _params) do
+      text(conn, "Subdomain home page for #{conn.private[:subdomain]}")
 
   end
 
-  def detail(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id}) do
     IO.inspect(id)
     text(conn, "Subdomain home page for #{conn.private[:subdomain]}")
   end
